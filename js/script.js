044fdc201aca14819fc607dba0261ab7b68e1fe7
Marcin Kubala <marcin.kubala.89@gmail.com>
2022-12-09 12:22:24 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let buttonElement = document.querySelector(".js-button");
+let body = document.querySelector(".js-body");
+let nameTheme = document.querySelector(".js-buttonName")
+
+buttonElement.addEventListener("click", () => {
+    body.classList.toggle("darkTheme");
+    nameTheme.innerText = body.classList.contains("darkTheme") ? "jasny" : "ciemny";
+});
